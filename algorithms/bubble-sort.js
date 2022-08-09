71471a8aa3cc0f10f4c8014239ab64aa7daaf2db
jonathan <jonathanlambert247@gmail.com>
2022-08-09 12:03:14 -0700

    init

---
+++ algorithms/bubble-sort.js
@@ -0,0 +1,15 @@
+// swap values until sorted from low-high
+const bubbleSort = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([8, 34, 12, 71, 29, 2, 120, 0]));
